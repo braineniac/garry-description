a9bdfb2a3dc5d3416f269a2edcdbdfed5492bc9d
Daniel Hammer <braineniac@protonmail.com>
2019-01-21 19:55:54 +0100

    Connected the state publisher to the imu

--- src/state_publisher.cpp
+++ src/state_publisher.cpp
@@ -1,84 +1,77 @@
 #include <string>
+#include <vector>
+
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/TwistStamped.h>
 
-#define WHEEL_RADIUS 2.7
+class StatePublisher {
+    public:
+        StatePublisher();
+        
+        void publish_odom_transform();
+
+    private:
+        ros::NodeHandle nh;
+        ros::Publisher joint_pub;
+        ros::Subscriber imu_sub;
+        tf::TransformBroadcaster broadcaster;
+
+        void imu_cb(const geometry_msgs::TwistStamped::ConstPtr& msg);
+
+        geometry_msgs::TwistStamped imu_pos;
+
+        std::vector<double> odom_pos_linear  = std::vector<double>(3);
+        std::vector<double> odom_pos_angular = std::vector<double>(3);
+};
+
+StatePublisher::StatePublisher() {
+    joint_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
+    imu_sub   = nh.subscribe<geometry_msgs::TwistStamped>("/imu_pos", 10, &StatePublisher::imu_cb, this);
+}
+
+void StatePublisher::imu_cb(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+
+    StatePublisher::imu_pos.header = msg->header;
+    StatePublisher::imu_pos.twist  = msg->twist;
+
+    StatePublisher::odom_pos_linear[0] += msg->twist.linear.x;
+    StatePublisher::odom_pos_linear[1] += msg->twist.linear.y;
+    StatePublisher::odom_pos_linear[2] += 0; // skipping this, since garry doesn't fly
+
+    StatePublisher::odom_pos_angular[0] += 0; // skipping since there is no tilt forward
+    StatePublisher::odom_pos_angular[1] += 0; // skipping since there is no tilt to the side
+    StatePublisher::odom_pos_angular[2] += msg->twist.angular.z;
+}
+
+void StatePublisher::publish_odom_transform() {
+
+    geometry_msgs::TransformStamped odom_to_base_trans;
+
+    odom_to_base_trans.header.stamp = ros::Time::now();
+    odom_to_base_trans.header.frame_id="odom";
+    odom_to_base_trans.child_frame_id="base_link";
+
+    odom_to_base_trans.transform.translation.x = StatePublisher::odom_pos_linear[0];
+    odom_to_base_trans.transform.translation.y = StatePublisher::odom_pos_linear[1];
+    odom_to_base_trans.transform.rotation = tf::createQuaternionMsgFromYaw(StatePublisher::odom_pos_angular[2]);
+
+    StatePublisher::broadcaster.sendTransform(odom_to_base_trans);
+}
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "state_publisher");
-	ros::NodeHandle n;
-	ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
-	tf::TransformBroadcaster broadcaster;
-	ros::Rate loop_rate(30);
+    ros::init(argc, argv, "state_publisher_node");
+    StatePublisher state_publisher_node;
+    ros::Rate loop_rate(10);
 
-	const double degree = M_PI/180;
+    while (ros::ok()) {
 
-	 // robot state
-	 double tilt = 0, tinc = degree, swivel=0, angle=0, height=0, hinc=0.005;
+        state_publisher_node.publish_odom_transform();
 
-	 // message declarations
-	 sensor_msgs::JointState joint_state;
-	 geometry_msgs::TransformStamped odom_trans_right_wheel;
-	 geometry_msgs::TransformStamped odom_trans_left_wheel;
-	 geometry_msgs::TransformStamped odom_trans_base;
-	 
-	 odom_trans_base.header.frame_id="odom";
-	 odom_trans_base.child_frame_id="base_link";
-	 odom_trans_right_wheel.header.frame_id = "right_gearbox";
-	 odom_trans_right_wheel.child_frame_id = "right_wheel";
- 	 odom_trans_left_wheel.header.frame_id = "left_gearbox";
-	 odom_trans_left_wheel.child_frame_id = "left_wheel";
+        loop_rate.sleep();
+    }
 
-
-	 while (ros::ok()) {
-		 //update joint_state
-		 
-		 joint_state.header.stamp = ros::Time::now();
-		 joint_state.name.resize(1);
-		 joint_state.position.resize(1);
-		 joint_state.name[0] ="swivel";
-		 joint_state.position[0] = swivel;
-		 
-		 odom_trans_base.header.stamp = ros::Time::now();
-		 odom_trans_base.transform.translation.x = 0;
-		 odom_trans_base.transform.translation.y = sqrt(2*2.7*2.7*(1-(cos(angle))));
-		 odom_trans_base.transform.translation.z = 0;
-		 odom_trans_base.transform.rotation = tf::createQuaternionMsgFromYaw(0);
-
-
-
-		 odom_trans_right_wheel.header.stamp = ros::Time::now();
-		 odom_trans_right_wheel.transform.translation.x = 4.575;
-		 odom_trans_right_wheel.transform.translation.y = -4.5;
-		 odom_trans_right_wheel.transform.translation.z = -3.3;
-		 odom_trans_right_wheel.transform.rotation = tf::createQuaternionMsgFromRollPitchYaw(-angle+M_PI/2,0,0);
-
-		 odom_trans_left_wheel.header.stamp = ros::Time::now();
-		 odom_trans_left_wheel.transform.translation.x = -4.575;
-		 odom_trans_left_wheel.transform.translation.y = -4.5;
-		 odom_trans_left_wheel.transform.translation.z = -3.3;
-		 odom_trans_left_wheel.transform.rotation = tf::createQuaternionMsgFromRollPitchYaw(-angle+M_PI/2,0,0);
-
-		 //send the joint state and transform
-		 joint_pub.publish(joint_state);
-		 broadcaster.sendTransform(odom_trans_base);
-		 broadcaster.sendTransform(odom_trans_right_wheel);
-		 broadcaster.sendTransform(odom_trans_left_wheel);
-
-
-		 // Create new robot state
-//		 tilt += tinc;
-//		 if (tilt<-.5 || tilt>0) tinc *= -1;
-//		 height += hinc;
-//		 if (height>.2 || height<0) hinc *= -1;
-		 swivel += degree;
-		 angle += degree/4;
-
-		 // This will adjust as needed per iteration
-		 loop_rate.sleep();
-	 }
-
-	 return 0;
+    return 0;
 
 }
